2c98a0cbb66cdf5258eab303df3ccb0af9b3ef10
joseLeyder <32853846+joseLeyder@users.noreply.github.com>
2019-08-19 13:27:04 -0500

    ley

--- CIDFares.Spa.WFApplication/Program.cs
+++ CIDFares.Spa.WFApplication/Program.cs
@@ -16,7 +16,7 @@ namespace CIDFares.Spa.WFApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
