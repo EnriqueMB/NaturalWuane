8dd9765580e9c689a4960f085c42b58a4e22d272
JuanMa2102 <40833994+JuanMa2102@users.noreply.github.com>
2019-08-20 14:09:05 -0500

    Juanito prueba pruebas

---
+++ CIDFares.Spa.Business/ViewModels/Catalogos/FormaPagoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDFares.Spa.Business.ViewModels.Catalogos
+{
+    public class FormaPagoViewModel
+    {
+        #region Propiedades
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Metodos
+        #endregion
+    }
+}
