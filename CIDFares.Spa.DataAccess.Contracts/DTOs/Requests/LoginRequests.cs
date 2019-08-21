e706a057ee97c2890e90ae519a72a0bdcdc65eca
joseLeyder <32853846+joseLeyder@users.noreply.github.com>
2019-08-21 11:12:13 -0500

    leyder

---
+++ CIDFares.Spa.DataAccess.Contracts/DTOs/Requests/LoginRequests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDFares.Spa.DataAccess.Contracts.DTOs.Requests
+{
+    public class LoginRequests
+    {
+        public LoginRequests()
+        {
+            IsValid = 0;
+        }
+
+        public int? IsValid { get; set; }
+
+        public int IdCuentaUsuario { get; set; }
+
+        public int IdRol { get; set; }
+
+        public Guid IdEmpleado { get; set; }
+
+        public string Nombres { get; set; }
+
+    }
+}
---
+++ CIDFares.Spa.WFApplication/Forms/General/FrmLogin.cs
@@ -0,0 +1,33 @@
+﻿
+using Syncfusion.Windows.Forms;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIDFares.Spa.WFApplication.Forms.General
+{
+    public partial class FrmLogin : MetroForm
+    {
+
+        public FrmLogin()
+        {
+           
+        }
+
+        
+        #region MoverFrom
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void PanelImagenMovimiento_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        #endregion
+    }
+}
