c5e56af96690f0583b8e3f6da4941c4ac406c147
JuanMa2102 <40833994+JuanMa2102@users.noreply.github.com>
2019-08-21 12:44:23 -0500

    Repository

--- CIDFares.Spa.Business/ViewModels/Catalogos/FormaPagoViewModel.cs
+++ CIDFares.Spa.Business/ViewModels/Catalogos/FormaPagoViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CIDFares.Spa.DataAccess.Contracts.Entities;
+using CIDFares.Spa.DataAccess.Contracts.Repositories.General;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +11,42 @@ namespace CIDFares.Spa.Business.ViewModels.Catalogos
 {
     public class FormaPagoViewModel
     {
-        #region Propiedades
+        #region Propiedades privadas
+        private IFormaPagoRepository Repository { get; set; }
+        #endregion
+
+        #region Propiedades públicas
+        public BindingList<FormaPago> ListaFormaPago { get; set; }
+        //public EntityState State { get; set; }
         #endregion
 
         #region Constructor
+        public FormaPagoViewModel(IFormaPagoRepository formaPagoRepository)
+        {
+            Repository = formaPagoRepository;
+            ListaFormaPago = new BindingList<FormaPago>();
+            GetAll();
+        }
         #endregion
 
         #region Metodos
+        public async Task GetAll()
+        {
+            try
+            {
+                var x = await Repository.GetAllAsync();
+                ListaFormaPago.Clear();
+                foreach (var item in x)
+                {
+                    ListaFormaPago.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
         #endregion
     }
 }
---
+++ CIDFares.Spa.DataAccess/Repositories/General/FormaPagoRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CIDFares.Spa.DataAccess.Contracts.Entities;
+using CIDFares.Spa.DataAccess.Contracts.Repositories;
+using CIDFares.Spa.DataAccess.Contracts.Repositories.General;
+
+namespace CIDFares.Spa.DataAccess.Repositories.General
+{
+    public class FormaPagoRepository : IFormaPagoRepository
+    {
+        public Task<FormaPago> AddAsync(FormaPago element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<FormaPago>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FormaPago> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FormaPago> UpdateAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
