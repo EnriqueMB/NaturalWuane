10058ba7cc615b065f8239ff3a2f0d1f2f0d021c
JuanraC <juanch00@live.com.mx>
2019-08-22 16:43:38 -0500

    Jr
    Jr

---
+++ CIDFares.Spa.Business/ViewModels/Catalogos/CategoriaProductoViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDFares.Spa.Business.ViewModels.Catalogos
+{
+    class CategoriaProductoViewModel
+    {
+        #region Propiedades privadas
+        private IClienteRepository Repository { get; set; }
+        #endregion
+
+        #region Propiedades públicas
+        public BindingList<Cliente> ListaCliente { get; set; }
+        public EntityState State { get; set; }
+        #endregion
+
+        #region Constructor
+        public ClienteViewModel(IClienteRepository formaPagoRepository)
+        {
+            Repository = formaPagoRepository;
+            ListaCliente = new BindingList<Cliente>();
+            GetAll();
+        }
+        #endregion
+
+        #region Metodos
+        public async Task GetAll()
+        {
+            try
+            {
+                var x = await Repository.GetAllAsync();
+                ListaCliente.Clear();
+                foreach (var item in x)
+                {
+                    ListaCliente.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region Binding(Variables)
+        private string _NombreCompleto;
+        public string NombreCompleto
+        {
+            get { return _NombreCompleto; }
+            set
+            {
+                _NombreCompleto = value;
+                OnPropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        #region InotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+        #endregion
+    }
+}
