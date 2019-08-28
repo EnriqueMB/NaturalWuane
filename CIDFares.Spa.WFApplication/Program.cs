7a75c25a6322edc6d9b7fe846fc31feae803fe4e
JuanMa2102 <40833994+JuanMa2102@users.noreply.github.com>
2019-08-28 11:50:03 -0500

    Update Program.cs

--- CIDFares.Spa.WFApplication/Program.cs
+++ CIDFares.Spa.WFApplication/Program.cs
@@ -29,7 +29,7 @@ namespace CIDFares.Spa.WFApplication
             Application.SetCompatibleTextRenderingDefault(false);
             InitializeContainer();
             CurrentSession.IdCuentaUsuario = Guid.Empty;
-            Application.Run(new FrmCliente());
+            Application.Run(new FrmVenta());
         }
 
         public static void InitializeContainer()
