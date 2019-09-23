1de6c18b222950ee8efd3856959fd61e270f2f0b
Alexis <Alexis.montoya97@hotmail.com>
2019-09-23 16:13:08 -0500

    ViewModel, repositorios

---
+++ CIDFares.Spa.Business/ViewModels/General/InventarioFisicoViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CIDFares.Spa.DataAccess.Contracts.Entities;
+using CIDFares.Spa.DataAccess.Contracts.Repositories.General;
+using CIDFares.Spa.DataAccess.Contracts.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDFares.Spa.Business.ViewModels.General
+{
+    public class InventarioFisicoViewModel : Validable, INotifyPropertyChanged
+    {
+        #region propiedades privadas
+        public IInventarioFisicoRepository IRepository { get; set; }
+        #endregion
+
+        #region propiedades publicas
+
+        #endregion
+
+        #region constructor
+        public InventarioFisicoViewModel(IInventarioFisicoRepository Repo)
+        {
+            IRepository = Repo;
+        }
+        #endregion
+
+        #region Metodos
+        public async Task<IEnumerable<Producto>> GetProducto(object Idsucursal)
+        {
+            try
+            {
+                return await IRepository.GetProductos(Idsucursal);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex ;
+            }
+        }
+        #endregion
+
+        #region Binding
+
+        #endregion
+        #region InotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
